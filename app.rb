7982d279374780199a687a0a57db5214c5189aec
Eli Cusic <tntitans31@gmail.com>
2020-09-28 17:03:11 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session, "secret"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        # binding.pry
+        session[:item] = params[:item]
+        @item = params[:item]
+        erb :index
+    end
 end
